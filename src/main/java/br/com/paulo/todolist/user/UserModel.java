678b867dbebb74f8a0e9e8bf31388fd57d6b3e9b
Paulo Roberto Portela Ribeiro <paulo.portela@opensingular.com>
2023-10-17 14:05:54 -0300

    criação readme.md

--- src/main/java/br/com/paulo/todolist/user/UserModel.java
+++ src/main/java/br/com/paulo/todolist/user/UserModel.java
@@ -1,29 +1,8 @@
 package br.com.paulo.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     private String username, name, password;
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
