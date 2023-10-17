1d83e31937cdc6bec6872c1bc9e804bb82c0ebc1
Paulo Roberto Portela Ribeiro <paulo.portela@opensingular.com>
2023-10-17 11:40:36 -0300

    estrutura projeto

---
+++ src/main/java/br/com/paulo/todolist/user/UserModel.java
@@ -0,0 +1,29 @@
+package br.com.paulo.todolist.user;
+
+public class UserModel {
+    private String username, name, password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
