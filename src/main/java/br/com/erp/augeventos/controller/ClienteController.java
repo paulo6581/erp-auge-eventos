91570ee43141e27d4a878ab7f7d08d3d2a02be05
Paulo Roberto <89169516+paulo6581@users.noreply.github.com>
2023-11-17 17:17:48 -0300

    Modificado ClienteController.java
    Arquivo modificado para integração com swagger

--- src/main/java/br/com/erp/augeventos/controller/ClienteController.java
+++ src/main/java/br/com/erp/augeventos/controller/ClienteController.java
@@ -2,6 +2,9 @@ package br.com.erp.augeventos.controller;
 
 import br.com.erp.augeventos.model.ClienteModel;
 import br.com.erp.augeventos.model.repository.IClienteRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +31,12 @@ public class ClienteController {
     public ClienteModel createCliente(@RequestBody ClienteModel clienteModel) {
         return clienteRepository.save(clienteModel);
     }
+
+    @Operation(description = "Busca o cliente pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o cliente"),
+            @ApiResponse(responseCode = "400", description = "Não existe o cliente com o id informado")
+    })
 
     @PutMapping("/{id}")
     public ClienteModel updateCliente(@PathVariable int id, @RequestBody ClienteModel clienteModel) {
